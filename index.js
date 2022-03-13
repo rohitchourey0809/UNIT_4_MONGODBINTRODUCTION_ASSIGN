34b25c42cdf0d56581b1bf8c6e420cf983306ad7
ROHIT CHOUREY <97465195+rohitchourey0809@users.noreply.github.com>
2022-03-13 14:43:57 +0530

    all okk

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+//CRUD
+//Creating----------> 
+step1--> use Assignment 
+step2--> db--> Assignment
+step3--> db.createCollection("USER")
+db.USER.insertMany([{first_name: "Dwight", "last_name": "Merriman","email":"R@123","gender":"male","ip_address":"192.168","age":"33"},{FirstName:"Kevin Ryan ", LastName:"Kevin Ryan "}])
+
+
+db.USER.find().pretty()
+db.USER.findOne().pretty()
+db.USER.remove({"_id":ObjectId("622db05da330a9539e4c348c")})
+db.USER.remove({first_name: "Dwight", "last_name": "Merriman","email":"R@123","gender":"male","ip_address":"192.168","age":"33"})
+db.dropDatabase()
+
+//reading
+//
